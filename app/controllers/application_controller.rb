44367987a798b618f4a8fb7b51919036665fedb9
Charles <charlie.david@me.com>
2022-11-17 17:42:31 -0700

    Back end commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,45 @@
+require 'pry'
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get "/post" do
+    post = Post.all
+    post.to_json
   end
 
+ get '/posts/:id' do
+  post = Post.find_by(id: params[:id])
+  post.to_json
+ end
+
+
+  patch  "/posts/:id" do
+    post = Post.find(params[:id])
+  post.update(
+    like_button: params[:like_button]
+  )
+  post.to_json
+  end 
+
+  delete  "/posts/:id" do 
+      post = Post.find(params[:id])
+      post.destroy
+      post.to_json
+    end
+
+  post '/posts' do
+    post = Post.create(
+      phase: params[:phase],
+      topic: params[:topic],
+      location: params[:location],
+      thread_post: params[:thread_post]
+    )
+    post.to_json
+  end
+
+  private
+  # def post_params
+
+  # end
 end
--- config.ru
+++ config.ru
@@ -4,7 +4,9 @@ require_relative "./config/environment"
 use Rack::Cors do
   allow do
     origins '*'
-    resource '*', headers: :any, methods: [:get, :post, :delete, :put, :patch, :options, :head]
+    resource '*', 
+    headers: :any,
+    methods: [:get, :post, :delete, :put, :patch, :options, :head]
   end
 end
 
